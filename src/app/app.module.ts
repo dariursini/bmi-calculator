17aaa835d29c0ba0cf98860816412f7f42761326
Dariane <dariursini@gmail.com>
2019-09-30 19:59:58 +0200

    Add angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormComponent } from './patient/form/form.component';
 import { TableComponent } from './patient/table/table.component';
 import { PatientService } from './patient.service';
+import { PatientsComponent } from './patient/patients.component';
+
+const appRoutes: Routes = [
+  { path: '', component: PatientsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FormComponent,
-    TableComponent
+    TableComponent,
+    PatientsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [PatientService],
   bootstrap: [AppComponent]
